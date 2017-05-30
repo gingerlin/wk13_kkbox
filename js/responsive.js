24fe774ab35123f8f20c3ca7e97d581f4556aea7
Len <a78965@gmail.com>
2017-05-30 20:16:06 +0800

    Fix redirect in smartphone device

--- js/responsive.js
+++ js/responsive.js
@@ -1,26 +1,29 @@
 $(function() {
 
     var width = $( window ).width();
+    var scale = $( '#scale' ).val();
     var MIN_WIDTH = 320;
     var MAX_WIDTH = 768;
 
 
     function detect_view() {
+        console.log(scale + ':' + width)
 
         // Mobile
         if (width <= MIN_WIDTH) {
 
-            if (scale != 'S') location.replace('../S');
+            if (scale != 'S') location.href = '../S';
 
         // Pad
         } else if (width > MIN_WIDTH && width <= MAX_WIDTH) {
 
-            if (scale != 'M') location.replace('../M');
+            if (scale != 'M') location.href = '../M';
 
         // PC
         } else {
 
-            if (scale != 'L') location.replace('../L');
+            if (scale != 'L') location.href = '../L';
+
         }
 
     }
